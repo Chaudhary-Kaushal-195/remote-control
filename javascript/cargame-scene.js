d4899a24b0e661585da6766b012b0bfa2e2d3431
Kaushal Chaudhary <kaushalchau.2007@gmail.com>
2026-02-27 15:21:19 +0530

    Reinstate realistic speed formula mapping 500 KPH max to 3D scene

--- javascript/cargame-scene.js
+++ javascript/cargame-scene.js
@@ -238,8 +238,31 @@ function animate() {
     let engineData = null;
     if (window.getEngineData) engineData = window.getEngineData();
 
+    // Max achievable KPH per gear at 10000 RPM
+    const maxGearSpeedsKPH = {
+        '-1': 60,   // Reverse
+        '0': 0,     // Neutral
+        '1': 100,
+        '2': 180,
+        '3': 260,
+        '4': 340,
+        '5': 420,
+        '6': 500
+    };
+
     if (engineData) {
-        speed = engineData.omega * 0.05;
+        let currentGear = engineData.gear.toString();
+        let maxGearKPH = maxGearSpeedsKPH[currentGear] || 0;
+
+        let calculatedKPH = (engineData.rpm / 10000) * maxGearKPH;
+
+        // Convert realistic KPH to ThreeJS scene speed (internal speed = KPH / 3.6)
+        if (engineData.gear === -1) {
+            speed = -calculatedKPH / 3.6;
+        } else {
+            speed = calculatedKPH / 3.6;
+        }
+
         rpm = engineData.rpm;
     } else {
         if (window.inputs && window.inputs.fwd) speed += 0.025;
